c2631cd93727701d571fe1db91abe1934aa79ae3
Jayjeet Chakraborty <jc.github@rediffmail.com>
2018-10-31 01:07:04 +0530

    Unlocked event

--- oth/urls.py
+++ oth/urls.py
@@ -4,11 +4,11 @@ from oth import views
 
 
 urlpatterns = [
-    url(r'^$', views.landing, name='landing'),
-    # url(r'^$', views.index, name='index'),
-    # url(r'^answer/$', views.answer , name='answer'),
-    # url(r'^lboard/$', views.lboard , name='lboard'),
-    # url(r'^rules/$', views.rules , name='rules'),
-    # url(r'^api/leaderboard/$',views.leaderboard_api,name='leaderboard_api'),
+    #url(r'^$', views.landing, name='landing'),
+    url(r'^$', views.index, name='index'),
+    url(r'^answer/$', views.answer , name='answer'),
+    url(r'^lboard/$', views.lboard , name='lboard'),
+    url(r'^rules/$', views.rules , name='rules'),
+    url(r'^api/leaderboard/$',views.leaderboard_api,name='leaderboard_api'),
 ]
 
