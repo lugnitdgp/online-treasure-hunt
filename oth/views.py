36f4e7d5077432931a848daafd3679d7eb8bbd8a
Jayjeet Chakraborty <jc.github@rediffmail.com>
2018-11-01 10:51:17 +0530

    fuck

--- oth/views.py
+++ oth/views.py
@@ -23,13 +23,16 @@ def index(request):
             level = models.level.objects.get(l_number=player.current_level)
             print(request.path)
             print(level.l_number)
-            if request.path == '/home/' or level.l_number > 1:
+            if request.path == '/home/' or level.l_number > 1 or level.l_number > 11:
                 return render(request, 'question2.html', {'player': player, 'level': level})
+            elif level.l_number == 11 and lastlevel == 20:
+                return redirect('story')
             else:
                 return redirect('story')
         except models.level.DoesNotExist:
             if player.current_level > lastlevel:
-                return render(request, 'win.html', {'player': player})
+                # changes from win to wait
+                return render(request, 'wait.html', {'player': player})
             return render(request, 'wait.html', {'player': player})
 
     return render(request, 'index.html')
